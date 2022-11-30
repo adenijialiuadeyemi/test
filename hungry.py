f28bb712315250c0dc82e8f050ed5c686306ccb1
adenijialiuadeyemi <adenijialiuadeyemi@gmail.com>
2022-11-30 05:27:53 -0800

    Option for thirsty

--- hungry.py
+++ hungry.py
@@ -1,7 +1,10 @@
 hungry = input("Are you Hungry? ")
 if hungry == "yes":
     print("Eat Samosa")
+    print("Eat Pizza")
 else:
-    print("Do your homework")
-    
- 
+    thirsty = input("Are you Thirsty? ")
+    if thirsty == "yes":
+        print("Drink water")
+    else:
+        print("You are on your own")
