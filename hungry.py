4f1d796de72cfd375e531ffb58cbbfbda946e755
adenijialiuadeyemi <adenijialiuadeyemi@gmail.com>
2022-11-30 05:36:28 -0800

    Updated main

--- hungry.py
+++ hungry.py
@@ -1,7 +1,7 @@
 hungry = input("Are you Hungry? ")
 if hungry == "yes":
     print("Eat Samosa")
+    print("Eat Egg")
 else:
-    print("Do your homework")
-    
- 
+    print("Do your Homewor")
+    print("Take some rest")
